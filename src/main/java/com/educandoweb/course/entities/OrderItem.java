abf94fe3d18a643eb13aa7b2e00dafa1abb36df2
Vanessa <vanessarcoin@gmail.com>
2024-02-16 21:57:17 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -32,6 +32,10 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 	
+	public Double getSubTotal() {
+		return price * quantity;
+ 	}
+	
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
@@ -64,7 +68,7 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
